df78b3781c9b6984a9c19b6589ba40be1212cd75
Janet Milagros <janet.huacahuasi.garcia@gmail.com>
2019-12-13 01:46:47 -0500

    chore: styled-components

--- client/src/components/Hello.tsx
+++ client/src/components/Hello.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    color: red;
+`;
 
 
-export interface HelloProps { compiler: string; framework: string; }
+export interface Props { compiler: string; framework: string; }
 
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+export const Hello: React.FC<Props> = ({compiler, framework}) => <Title>Hello from {compiler} and {framework}!</Title>;
